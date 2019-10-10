c54523e95af61b3c40c1dc7395c0fb192066bc0a
q1q0374 <55523902+q1q0374@users.noreply.github.com>
2019-10-10 16:04:53 +0900

    Add files via upload

---
+++ inc02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	
+	
+	a=6;
+	a--;
+	printf("%d",a);
+	--a;
+	printf("%d",a);
+	a=a-1;
+	printf("%d",a);
+	
+	
+	return 0;
+}
---
+++ shift.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a=3,b,c;
+	
+	b= a<<1;
+	printf("%d %d\n",a,b); //3*2^1
+	
+	c=a<<2;
+	printf("%d %d\n",a,c);//3*2^2
+	
+	
+	
+	return 0;
+}
